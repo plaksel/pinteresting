100a72dc6fc5291d5943aeca65ce704a1f8a9ef9
Rik D <rikvandonselaar@gmail.com>
2014-06-30 22:43:06 +0100

    validating images type and size

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,5 +2,8 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => {:medium =>"300x300>", :thumb => "100x100>"}
-	do_not_validate_attachment_file_type :image
+	#do_not_validate_attachment_file_type :image
+	validates_attachment :image, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
+	validates_attachment_size :image, :less_than=>1.megabyte
+
 end
