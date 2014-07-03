2daf990592772d6a65fddf9f46c2c8028c7e2f9c
Rik D <rikvandonselaar@gmail.com>
2014-07-03 13:18:50 +0100

    resize original image upload

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,9 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => {:medium =>"300x300>", :thumb => "100x100>"}
+	has_attached_file :image, :styles => {:original =>"1024x1024", :medium =>"300x300>", :thumb => "100x100>"}
 	#do_not_validate_attachment_file_type :image
 	validates_attachment :image, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
-	validates_attachment_size :image, :less_than=>1.megabyte
+	#validates_attachment_size :image, :less_than=>1.megabyte
 
 end
